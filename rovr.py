3fa48abe5be21745064f5525f3a5f9fa1ae2a092
Nick Pittman <34899577+nickschaos81@users.noreply.github.com>
2023-06-02 20:43:58 -0700

    Update rovr.py

--- rovr.py
+++ rovr.py
@@ -4,11 +4,6 @@ import cv2
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
-# Set camera resolution to 1280x720
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-
 def generate_frames():
     while True:
         success, frame = camera.read()
@@ -21,16 +16,13 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
 @app.route('/')
 def index():
     return render_template('display.html')
-
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', debug=True)
